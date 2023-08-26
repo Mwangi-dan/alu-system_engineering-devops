6e5bf0825027c12c44dafb371eb3e46e58818bb3
root <d.ndungu@alustudent.com>
2023-08-26 02:28:04 -0700

    The sky is the limit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Increases the amaount of traffic nginx server can handle
 
 exec { 'fix--for-nginx':
-	command => 'sed -i "s/15/4096/" /etc/default/nginx',
-	path => '/usr/local/bin/:/bin/'
-} ->
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/'
+}
 
-# Restar nginx
+# Restart nginx
 exec { 'nginx-restart':
-	command => 'nginx restart',
-	path => '/etc/init/d/'
+    command => 'nginx restart',
+    path    => '/etc/init/d/'
 }
